20f25036b3c978ae70824adaf5ee505097746e38
boubakermohamedali <72795659+boubakermohamedali@users.noreply.github.com>
2023-07-04 14:55:48 +0200

    modification

--- date.php
+++ date.php
@@ -29,23 +29,12 @@ echo date_format($date, 'Y-m-d H:i:s');
 $signes = array("Capricorne","Verseau","Poisson","Bélier","Taureau","Gémeaux","Cancer","Lion","Vierge","Balance","Scorpion","Sagittaire");
 //$age=array("31 28 30 33 38 20 14 10 36 80 12 22");
 $mois = ["January", "February"," March"," April","May","June","July","August","September","October","November","December"];
-if(($_POST['date_de_naissance']!='')){
-    $date= $_POST['date_de_naissance'];
-    $aujourdhui = date("Y-m-d");
-    $diff = date_diff(date_create($date), date_create($aujourdhui));
-    echo 'Votre age est '.$diff->format('%y');
-    echo"<br>";
-    $signes_astro= array_combine($mois, $signes);
-    var_dump($signes_astro);
-    print_r($signes_astro[$Mois]);}
-    echo"<br>";
-    //echo date("d/m/Y"); // Affiche la date du jour
-    //echo "Il est " .date("H:i:s") ; // Affiche l'heure
-    ?>
-
-
-  
+// Transforme un Timestamp Unix en date jj/mm/aaaa
+function Timestamp2Date($timestamp,$format)
+{
+if ($format == "") {
+$format="d/m/Y";
+}
+return date($format, $timestamp);
+}
 ?>
-
-
-            
